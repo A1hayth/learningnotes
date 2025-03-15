93497dfb26a6b41bc88a0d9e706f3c985abfd746
Alhayth <3480969378@qq.com>
2025-03-15 22:18:44 +0800

    2025.3.15-2

--- C/算法/sort.cpp
+++ C/算法/sort.cpp
@@ -19,6 +19,26 @@ void selectionSort(int a[],int N)
     }
   }
 }
+
+//插入排序
+void insertionSort(int a[],int n)
+{
+    for(int i = 1; i < n; i++)
+  {
+    int key = a[i];//设置哨兵
+    int j = i - 1;
+
+    //将比key大的元素都向后移动
+    while(j >= 0 && a[j] > key)
+    {
+      a[j + 1] = a[j];
+      j--;
+    }
+    //将key插入正确位置
+    a[j + 1] = key;
+  }
+}
+
 int main()
 {
   return 0;
