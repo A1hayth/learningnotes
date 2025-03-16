074f1c7e1674e8b361ec4e9de01bd1213f5dee63
Alhayth <3480969378@qq.com>
2025-03-16 17:20:51 +0800

    2025.3.16

--- C/算法/sort.cpp
+++ C/算法/sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
-//选择排序
+
+//选择排序(升序)
 void selectionSort(int a[],int N)
 {
   for(int i = 0;i < N;i++)
@@ -20,7 +21,7 @@ void selectionSort(int a[],int N)
   }
 }
 
-//插入排序
+//插入排序(升序)
 void insertionSort(int a[],int n)
 {
     for(int i = 1; i < n; i++)
@@ -37,6 +38,52 @@ void insertionSort(int a[],int n)
     //将key插入正确位置
     a[j + 1] = key;
   }
+}
+
+//快速排序
+//划分函数
+int partition(int a[], int s, int t)
+{
+    int i = s, j = t;
+    int base = a[i];
+    while (i < j)
+    {
+        while (i < j && a[j] > base)
+        {
+            j--;
+        }
+        if (i < j)
+        {
+            a[i] = a[j];
+            i++;
+        }
+        while (i < j && a[i] < base)
+        {
+            i++;
+        }
+        if (i < j)
+        {
+            a[j] = a[i];
+            j--;
+        }
+    }
+    a[i] = base;
+    return i;  // 修正返回值为 i
+}
+
+//递归实现左右子表的排序
+void _quicksort(int a[], int s, int t)
+{
+    if (s < t) {
+        int i = partition(a, s, t);
+        _quicksort(a, s, i - 1);  // 左子表递归
+        _quicksort(a, i + 1, t);  // 右子表递归
+    }
+}
+//调用
+void quicksort(int a[], int t)
+{
+    _quicksort(a, 0, t);
 }
 
 int main()
