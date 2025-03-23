56f121dbba0b39e325520948b98c6b20385ce442
Alhayth <3480969378@qq.com>
2025-03-23 23:52:17 +0800

    2025.3.23

---
+++ C/learn/1.cpp
@@ -0,0 +1,48 @@
+//#include<iostream>
+//#include<cstring>
+//using namespace std;
+//
+//class Ball
+//{
+//private:
+//	double r;
+//public:
+//	void setR(double x)
+//	{
+//		r = x;
+//	}
+//	double getR()
+//	{
+//		return r;
+//	}
+//};
+//
+//class Billiards :public Ball
+//{
+//private :
+//	string color;
+//public:
+//	void setCol(string col)
+//	{
+//		color = col;
+//	}
+//	void show()
+//	{
+//		cout << "r = " << getR() << endl;
+//		cout << "color is " << color << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	Billiards b1;
+//	b1.setR(1);
+//	b1.setCol("red");
+//	Billiards b2;
+//	b2.setR(2);
+//	b2.setCol("green");
+//	b1.show();
+//	b2.show();
+//
+//	return 0;
+//}
